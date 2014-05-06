cf6df6d74f011012ad894c523afb494a6c4b1e9d
Sparklehorse <heyulin0416@163.com>
2014-05-06 21:10:10 +0800

    v2
    add set to remove duplicate URL

--- mm_crawler.py
+++ mm_crawler.py
@@ -7,11 +7,10 @@ import inspect
 import getopt
 import queue
 import threading
-
+from bs4 import BeautifulSoup
 
 class MyThread(threading.Thread):
     def __init__(self, myQueue, picPath):  # passing myQueue
-        #print(myQueue.get())
         print("init")
         threading.Thread.__init__(self)
         self.myQueue = myQueue
@@ -34,6 +33,87 @@ class MyThread(threading.Thread):
                 print("finished")
                 break
 
+
+class MyUrlThread(threading.Thread):
+    def __init__(self, myUrlQueue, mySeenQueue, myPicGroupQueue, picNum):  # passing myQueue
+        print("init")
+        threading.Thread.__init__(self)
+        self.myUrlQueue = myUrlQueue
+        self.mySeenQueue = mySeenQueue
+        self.myPicGroupQueue = myPicGroupQueue
+        self.picNum = picNum
+        #self.initUrl = "http://www.22mm.cc/"
+    def run(self):
+        print("crawler")
+        #contentUrlList = []
+        #columnUrlList = []
+        #indexUrlList = []
+        while True:
+            if self.myUrlQueue.empty() is False:
+                url = self.myUrlQueue.get()
+                innerUrlHtml = getHtml(url)
+                soup = BeautifulSoup(innerUrlHtml)
+                urlList = soup.findAll('a', href=re.compile(r'/mm/\S+'))
+                rawIndexUrl = soup.findAll('a', href=re.compile(r'index\S+html'))
+                curUrl = soup.find(attrs={"class": "cur"})
+                print(curUrl)
+                print("cur", curUrl['href'])
+                if len(rawIndexUrl) != 0:  # http://www.22mm.cc/mm/qingliang/index.html
+                    print(rawIndexUrl)
+                    for i in range(0, len(rawIndexUrl)):
+                        indexUrl = "http://www.22mm.cc"+curUrl['href']+rawIndexUrl[i]['href']
+                        seen = self.mySeenQueue.get()
+                        if indexUrl not in seen:
+                            seen.add(indexUrl)
+                            self.mySeenQueue.put(seen)
+                            self.myUrlQueue.put(indexUrl)
+                        else:
+                            self.mySeenQueue.put(seen)
+                print("~~~~~~~~~~~~")
+                for i in range(0, len(urlList)):
+                    innerUrl = urlList[i]['href']
+                    innerUrl = "http://www.22mm.cc"+innerUrl
+                    if innerUrl.endswith(".html") is True and innerUrl.endswith("/rec.html") is False:  # http://www.22mm.cc/mm/qingliang/PiaidemeJJPaimmHm.html
+                        seen = self.mySeenQueue.get()
+                        if innerUrl not in seen:
+                            seen.add(innerUrl)
+                            self.mySeenQueue.put(seen)
+                            innerHtml = getHtml(innerUrl)
+                            pattern = re.compile(r'</span>/\d+</strong>')
+                            innerHtmlNumSpan = pattern.findall(innerHtml)[0]
+                            innerHtmlNumSpan = innerHtmlNumSpan.replace('</span>/', '')
+                            innerHtmlNumStr = innerHtmlNumSpan.replace('</strong>', '')
+                            innerHtmlNum = int(innerHtmlNumStr)
+                            picHtml = innerUrl[0:-5]+"-"+str(innerHtmlNum)+".html"
+                            if self.picNum != -1 and self.myPicGroupQueue.qsize() <= self.picNum/10:
+                                self.myPicGroupQueue.put(picHtml)
+                            elif self.picNum == -1:
+                                self.myPicGroupQueue.put(picHtml)
+                            else:
+                                break
+                        else:
+                            self.mySeenQueue.put(seen)
+                        #contentUrlList.append(innerUrl)
+                    else:  # http://www.22mm.cc/mm/jingyan/
+                        seen = self.mySeenQueue.get()
+                        if innerUrl not in seen:
+                            seen.add(innerUrl)
+                            self.mySeenQueue.put(seen)
+                            self.myUrlQueue.put(innerUrl)
+                        else:
+                            self.mySeenQueue.put(seen)
+                #print(contentUrlList)
+                #print(columnUrlList)
+                print("=============")
+                print("", self.myUrlQueue.qsize())
+                print("", self.mySeenQueue.qsize())
+                print("size", self.myPicGroupQueue.qsize())
+            else:
+                break
+        #return
+        callback()
+
+
 '''
 def reporthook(a, b, c):
     #print(a,b,c)
@@ -42,16 +122,23 @@ def reporthook(a, b, c):
         percent = 100
     print("%d %d"%(percent, (count+(a*b/c))/len(myItems)*100))
 '''
-
+def callback():
+    print("done")
 
 def getHtml(url):
-    fp = urllib.request.urlopen(url)
+    try:
+        fp = urllib.request.urlopen(url)
+    except Exception as err:
+        print("err", err)
+        fp = urllib.request.urlopen(url)
     mybytes = fp.read()
     # note that Python3 does not read the html code as string
     # but as html code bytearray, convert to string with
     html = mybytes.decode("utf8")
     fp.close()
-    return html
+    if html is not None:
+        return html
+
 
 
 def getHtmlList():
@@ -84,6 +171,12 @@ def getImgList(htmlList):
             rawImgList[n] = rawImgList[n].replace('big', 'pic')
             imgList.append(rawImgList[n])
     return imgList
+
+
+def getUrl(myUrlQueue, mySeenQueue, myPicGroupQueue, argvList):
+    for i in range(0, argvList[0]):
+        myUrlThread = MyUrlThread(myUrlQueue, mySeenQueue, myPicGroupQueue,argvList[2])
+        myUrlThread.start()
 
 
 def download(myQueue, argvList):
@@ -122,7 +215,26 @@ def main():
     initOpt(argvList)
     if os.path.exists(argvList[1]) is False:  # make dir
         os.mkdir(argvList[1])
-    htmlList = getHtmlList()  # get htmlList
+    #htmlList = getHtmlList()  # get htmlList
+    initUrl = "http://www.22mm.cc/"
+    myUrlQueue = queue.Queue()
+    mySeenQueue = queue.Queue()
+    myPicGroupQueue = queue.Queue()
+    seen = set()
+    seen.add(initUrl)
+    myUrlQueue.put(initUrl)
+    mySeenQueue.put(seen)
+    print("siz",mySeenQueue.qsize())
+    #htmlList = getUrl(myUrlQueue, mySeenQueue, myPicGroupQueue, argvList)
+    getUrl(myUrlQueue, mySeenQueue, myPicGroupQueue, argvList)
+    '''
+    无法等线程结束后才对htmlList赋值
+    '''
+    htmlList = []
+    for i in range(0, myPicGroupQueue.qsize()):
+        htmlList = myPicGroupQueue.get()
+    print(htmlList)
+
     imgList = getImgList(htmlList)  # get pic URL
     print(imgList)
     myQueue = queue.Queue()  # init queue
